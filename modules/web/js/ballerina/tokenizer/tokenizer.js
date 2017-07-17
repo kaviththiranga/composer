6d145bfdeb67dabb026c3e4968c4b1dbe6d296ef
Kavith Lokuhewage <kaviththiranga@gmail.com>
2017-07-17 14:35:30 +0530

    Initial impl of tokenizer

--- modules/web/js/ballerina/tokenizer/tokenizer.js
+++ modules/web/js/ballerina/tokenizer/tokenizer.js
@@ -3,11 +3,21 @@ import { BallerinaLexer } from './gen/BallerinaLexer';
 import { BallerinaParser } from './gen/BallerinaParser';
 
 class BLangTokenizer {
-    constructor() {
+    constructor(args) {
+        this.content = '';
     }
 
     tokenize() {
+        const chars = new antlr4.InputStream(this.content);
+        const lexer = new BallerinaLexer(chars);
+        const tokens  = new antlr4.CommonTokenStream(lexer);
+        const parser = new BallerinaParser(tokens);
+        parser.buildParseTrees = true;
+        const tree = parser.compilationUnit();
+    }
 
+    getLineTokens(line, state) {
+        return [{ type: "", value: "", index: 0, start: 0}];
     }
 
 }
